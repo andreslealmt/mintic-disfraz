ac5327bc0dfa9de70394a70174bd8869e977fa8c
andreslealmt <andres.leal1.mt@correo.usa.edu.co>
2021-11-06 15:35:05 -0500

    prueba 1 dropdown

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -11,21 +11,21 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">                
-                <router-link to="/" class="nav-link" aria-current="page">Inicio</router-link>
+                    <router-link to="/" class="nav-link" aria-current="page">Inicio</router-link>
                 </li>               
                 <li class="nav-item dropdown">
 
-                <div class="dropdown">
-                        <a class="nav-link dropdown-toggle dropdown" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Categorias
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
-                            <li @click="cambioCategoria('todo')"><a class="dropdown-item" href="#">Todo</a></li>                    
-                            <li><hr class="dropdown-divider"></li>
-                            <li v-for="item in categorias" :key="item.id" @click="cambioCategoria(item.name)"><a class="dropdown-item" href="#">{{item.name}}</a></li>                                                                        
-                        </ul>
+                    <div class="dropdown">
+                            <a class="nav-link dropdown-toggle dropdown" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            categories
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdown">
+                                <li @click="cambioCategoria('todo')"><a class="dropdown-item" href="#">Todo</a></li>                    
+                                <li><hr class="dropdown-divider"></li>
+                                <li v-for="item in categorias" :key="item.id" @click="cambioCategoria(item.name)"><a class="dropdown-item" href="#">{{item.name}}</a></li>                                                                        
+                            </ul>
 
-                </div>
+                    </div>
                 
                 </li>
                  <li class="nav-item left" >
